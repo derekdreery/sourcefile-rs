a3f47b11a7c8316e68f20fe379e8f37d8352bd5a
Richard Dodd <richard.o.dodd@gmail.com>
2021-07-04 20:58:38 +0100

    Add raw adder method.
    Also remove possibility of inconsistent state by preloading file
    contents.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,8 @@
 //! A library providing `SourceFiles`, a concatenated list of files with information for resolving
 //! points and spans.
 
-use std::fs;
-use std::io::{self, BufRead};
 use std::path::Path;
-
-macro_rules! check_error {
-   ($res:expr, $this:expr) => {{
-        let out = $res;
-        if out.is_err() {
-            $this.error = true;
-        }
-        out
-   }};
-}
+use std::{fmt, fs, io};
 
 /// A concatenated string of files, with sourcemap information.
 #[derive(Debug, Default, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
@@ -26,12 +15,9 @@ pub struct SourceFile {
     file_lines: Vec<usize>,
     /// The length of each line in all source files
     line_lengths: Vec<usize>,
-    /// Keep track of whether the internal state is inconsistent.
-    error: bool,
 }
 
 impl SourceFile {
-
     /// Create a new empty sourcefile. Equivalent to `Default::default`.
     pub fn new() -> Self {
         Default::default()
@@ -40,35 +26,45 @@ impl SourceFile {
     /// Concatenate a file to the end of `contents`, and record info needed to resolve spans.
     ///
     /// If the last line doesn't end with a newline character, it will still be a 'line' for the
-    /// purposes of this calcuation.
-    ///
-    /// Consumes `self` because the structure would be inconsistent after an error.
+    /// purposes of this calculation.
     pub fn add_file(&mut self, filename: impl AsRef<Path>) -> io::Result<()> {
         let filename = filename.as_ref();
-        let mut file = io::BufReader::new(check_error!(fs::File::open(filename), self)?);
+        let file = fs::read_to_string(filename)?;
 
         // We should skip this file if it is completely empty.
-        let line_len = check_error!(file.read_line(&mut self.contents), self)?;
-        if line_len == 0 {
-            return Ok(());
-        }
-        self.line_lengths.push(line_len);
+        self.add_file_raw(filename.display(), file);
+        Ok(())
+    }
 
-        let mut num_lines = 1; // We already got one above.
-        loop {
-            let line_len = check_error!(file.read_line(&mut self.contents), self)?;
-            if line_len == 0 { //EOF
-                break;
+    pub fn add_file_raw(&mut self, name: impl fmt::Display, contents: impl Into<String>) {
+        let contents = contents.into();
+        // We should skip this file if it is completely empty (There are no offsets that index into this file).
+        if contents.is_empty() {
+            return;
+        }
+
+        let mut num_lines = 0;
+        // We can't use str::lines because we won't know if 1 or 2 chars were lost (if there was a \r).
+        let mut lines = contents.split('\n').peekable();
+        while let Some(line) = lines.next() {
+            if lines.peek().is_some() {
+                // middle line
+                num_lines += 1;
+                self.line_lengths.push(line.len() + 1);
+            } else if line.is_empty() {
+                // last line is empty, skip it
+            } else {
+                // last line not empty, but no \n at the end.
+                num_lines += 1;
+                self.line_lengths.push(line.len());
             }
-            self.line_lengths.push(line_len);
-            num_lines += 1;
         }
 
         // Record the name
-        self.file_names.push(format!("{}", filename.display()));
+        self.file_names.push(name.to_string());
         // Record the number of lines
         self.file_lines.push(num_lines);
-        Ok(())
+        self.contents += &contents;
     }
 
     /// Get the file, line, and col position of a byte offset.
@@ -77,9 +73,6 @@ impl SourceFile {
     ///
     /// This function will panic if `offset` is not on a character boundary.
     pub fn resolve_offset<'a>(&'a self, offset: usize) -> Option<Position<'a>> {
-        if self.error {
-            panic!("there was an error adding a file to this sourcefile")
-        }
         // If there isn't a single line, always return None.
         let mut line_acc = *self.line_lengths.get(0)?;
         let mut line_idx = 0;
@@ -101,16 +94,17 @@ impl SourceFile {
         // Go back to the start of the file (for working out the line).
         file_acc -= self.file_lines[file_idx];
 
-        Some(Position::new(&self.file_names[file_idx], line_idx - file_acc, offset - line_acc))
+        Some(Position::new(
+            &self.file_names[file_idx],
+            line_idx - file_acc,
+            offset - line_acc,
+        ))
     }
 
-    /// Get the file, line, and col position of each end of a span
+    /// Get the file, line, and col position of each end of a span.
     // TODO this could be more efficient by using the fact that end is after (and probably near to)
     // start.
     pub fn resolve_offset_span<'a>(&'a self, start: usize, end: usize) -> Option<Span<'a>> {
-        if self.error {
-            panic!("there was an error adding a file to this sourcefile")
-        }
         if end < start {
             return None;
         }
@@ -135,7 +129,11 @@ pub struct Position<'a> {
 impl<'a> Position<'a> {
     /// Constructor for tests.
     fn new(filename: &'a str, line: usize, col: usize) -> Position<'a> {
-        Position { filename: filename.as_ref(), line, col }
+        Position {
+            filename: filename.as_ref(),
+            line,
+            col,
+        }
     }
 }
 
@@ -150,8 +148,8 @@ pub struct Span<'a> {
 mod tests {
     extern crate tempfile;
 
-    use super::{SourceFile, Position, Span};
     use self::tempfile::NamedTempFile;
+    use super::{Position, SourceFile, Span};
     use std::io::Write;
 
     #[test]
@@ -163,36 +161,35 @@ mod tests {
     #[test]
     fn smoke() {
         test_files(
-            &["A file with\ntwo lines.\n",
-              "Another file with\ntwo more lines.\n",
+            &[
+                "A file with\ntwo lines.\n",
+                "Another file with\ntwo more lines.\n",
             ],
-            &[(0, (0, 0, 0)), // start
-              (5, (0, 0, 5)), // last char first line first file
-              (11, (0, 0, 11)), // first char second line first file
-              (12, (0, 1, 0)), // ..
-              (13, (0, 1, 1)),
-              (13, (0, 1, 1)),
-              (22, (0, 1, 10)),
-              (23, (1, 0, 0)),
-              (24, (1, 0, 1)),
-              (40, (1, 0, 17)),
-              (41, (1, 1, 0)),
-              (42, (1, 1, 1)),
-              (56, (1, 1, 15)),
-              //(57, (1, 1, 16)), // should panic
+            &[
+                (0, (0, 0, 0)),   // start
+                (5, (0, 0, 5)),   // last char first line first file
+                (11, (0, 0, 11)), // first char second line first file
+                (12, (0, 1, 0)),  // ..
+                (13, (0, 1, 1)),
+                (13, (0, 1, 1)),
+                (22, (0, 1, 10)),
+                (23, (1, 0, 0)),
+                (24, (1, 0, 1)),
+                (40, (1, 0, 17)),
+                (41, (1, 1, 0)),
+                (42, (1, 1, 1)),
+                (56, (1, 1, 15)),
+                //(57, (1, 1, 16)), // should panic
             ],
-            &[((0, 5), (0, 0, 0), (0, 0, 5)),
-            ]
+            &[((0, 5), (0, 0, 0), (0, 0, 5))],
         )
     }
 
-    fn test_files<'a>(files: &[impl AsRef<str>],
-                      offset_tests: &[(usize,
-                                       (usize, usize, usize))],
-                      offset_span_tests: &[((usize, usize),
-                                            (usize, usize, usize),
-                                            (usize, usize, usize))])
-    {
+    fn test_files<'a>(
+        files: &[impl AsRef<str>],
+        offset_tests: &[(usize, (usize, usize, usize))],
+        offset_span_tests: &[((usize, usize), (usize, usize, usize), (usize, usize, usize))],
+    ) {
         let mut sourcefile = SourceFile::default();
         let mut file_handles = Vec::new(); // don't clean me up please
         for contents in files {
@@ -205,21 +202,31 @@ mod tests {
         for &(offset, (file_idx, line, col)) in offset_tests {
             let filename = format!("{}", file_handles[file_idx].path().display());
             let pos = sourcefile.resolve_offset(offset);
-            assert!(pos.is_some());
             assert_eq!(pos.unwrap(), Position::new(&filename, line, col));
         }
 
-        for &((start, end),
-              (file_idx_start, line_start, col_start),
-              (file_idx_end, line_end, col_end)) in offset_span_tests
+        for &(
+            (start, end),
+            (file_idx_start, line_start, col_start),
+            (file_idx_end, line_end, col_end),
+        ) in offset_span_tests
         {
             let start_filename = format!("{}", file_handles[file_idx_start].path().display());
             let end_filename = format!("{}", file_handles[file_idx_end].path().display());
-            assert_eq!(sourcefile.resolve_offset_span(start, end).unwrap(),
-                       Span {
-                           start: Position::new(&start_filename, line_start, col_start),
-                           end: Position::new(&end_filename, line_end, col_end),
-                       });
+            assert_eq!(
+                sourcefile.resolve_offset_span(start, end).unwrap(),
+                Span {
+                    start: Position::new(&start_filename, line_start, col_start),
+                    end: Position::new(&end_filename, line_end, col_end),
+                }
+            );
         }
+    }
+
+    #[test]
+    fn test_raw() {
+        let mut sourcefile = SourceFile::new();
+        sourcefile.add_file_raw("test", " ");
+        assert_eq!(*sourcefile.line_lengths.last().unwrap(), 1);
     }
 }
